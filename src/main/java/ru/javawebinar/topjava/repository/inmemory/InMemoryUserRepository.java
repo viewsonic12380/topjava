cbe148c390a376460885e7ade618c2c853613ba4
admin <viewsonic12380@gmail.com>
2020-06-17 18:58:54 +0500

    Modify InMemoryUserRepository.java function getAll(): -make anonymous class -modify an.class with lambda -modify lambda with Stream API

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,15 +3,13 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.AbstractNamedEntity;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.util.NameComparator;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
@@ -38,8 +36,7 @@ public class InMemoryUserRepository implements UserRepository {
     @Override
     public List<User> getAll() {
         List<User> userList = new ArrayList<User>(repository.values());
-        NameComparator nameComparator = new NameComparator();
-        Collections.sort(userList, nameComparator);
+        Collections.sort(userList, Comparator.comparing(AbstractNamedEntity::getName));
         return userList;
     }
 
